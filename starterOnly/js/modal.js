60437ca8aafa8a2a2f1ee57ae4724fcf05fa39b0
Jérôme Abel <contact@jeromeabel.net>
2022-10-06 11:05:37 +0200

    Reset form inputs with closeModal()

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -10,7 +10,6 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector("span.close");
 
 // launch modal event
@@ -21,8 +20,29 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close Modal and Reset Form inputs
 function closeModal() {
+  // Hide Modal
   modalbg.style.display = "none";
+
+  // Remove Success Element
+  const elSuccess = document.querySelector(".success");
+  if (elSuccess) elSuccess.remove();
+
+  // Show form inputs 
+  document.querySelector('.text-label').style.display = "block";
+  const inputs = document.getElementsByClassName('formData');
+  for (const el of inputs) {
+      el.style.opacity = 1;
+      el.style.visibility = "visible";
+  }
+
+  // Btn Submit
+  document.querySelector(".btn-submit").addEventListener('click', () => true);
+
+  // Reset Form
+  document.querySelector("#form-signup").reset();
 }
+
 // Close modal
 closeBtn.addEventListener('click', closeModal);
